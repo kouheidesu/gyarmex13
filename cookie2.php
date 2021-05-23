97e31ed195f91e9803501ffe8d961b6da5f6a577
kouheidesu <gyarmex@gmail.com>
2021-05-23 11:10:53 +0900

    20210523 kazihara

--- cookie2.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <?php
-        $cookie=$_COOKIE['value'];
-        print "クッキーの値は" . $cookie . "です<br />";
-        ?>
-        <a href="cookie1.php">ページ1へ</a>
-    </body>
-</html>
--- course.php
+++
@@ -1,3 +0,0 @@
-<?php
-$company = htmlspecialchars($_POST['company'], ENT_QUOTES);
-print "会社名は" . $company . "ですね";
