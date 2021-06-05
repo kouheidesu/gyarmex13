2ef96e115c56a544812e52dcdd7e29d4251b0e12
kouheidesu <gyarmex@gmail.com>
2021-06-05 16:02:48 +0900

    20210605 kazihara

--- dbpj/database/seeders/PeopleTableSeeder.php
+++ dbpj/database/seeders/PeopleTableSeeder.php
@@ -2,29 +2,31 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder as DatabaseSeeder;
-use Illuminate\Databese\Seeder;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class PeopleTableSeeder extends Seeder
 {
-    
-
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
-        $param=[
-            'name'=>'taro',
-            'age'=>10,
+        $param = [
+            'name' => 'taro',
+            'age' => 10,
         ];
         DB::table('people')->insert($param);
-        $param=[
-            'name'=>'jiro',
-            'age'=>20,
+        $param = [
+            'name' => 'jiro',
+            'age' => 20,
         ];
         DB::table('people')->insert($param);
-        $param=[
-            'name'=>'saburo',
-            'age'=>30,
+        $param = [
+            'name' => 'saburo',
+            'age' => 30,
         ];
         DB::table('people')->insert($param);
     }
